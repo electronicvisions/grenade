24a40b766b2240011859a60a8d93c9bd1b368c8c
Philipp Spilger <philipp.spilger@kip.uni-heidelberg.de>
2020-09-09 10:49:25 +0200

    Adapt to v1 namespace in halco
    Depends-On: 11968
    Change-Id: I8a7bcaacd61116ab3df24e80ed44685c35c51080

--- include/grenade/vx/grenade.h
+++ include/grenade/vx/grenade.h
@@ -3,7 +3,7 @@
 GENPYBIND_TAG_GRENADE_VX
 GENPYBIND_MANUAL({
 	parent.attr("__variant__") = "pybind11";
-	parent->py::module::import("pyhalco_hicann_dls_vx");
+	parent->py::module::import("pyhalco_hicann_dls_vx_v1");
 	parent->py::module::import("pyhaldls_vx_v1");
 	parent->py::module::import("pylola_vx_v1");
 })
