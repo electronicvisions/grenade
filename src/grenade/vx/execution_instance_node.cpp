9039782f2cd9280f6cd7c7a5c9f7ded120523b53
Eric Müller <mueller@kip.uni-heidelberg.de>
2020-10-12 08:42:02 +0200

    Add experiment post-mortem analysis
    Adds log output for received highspeed link notifications
    and performs a post-mortem status readout.
    
    Change-Id: I2bab54eeaba22a506b4fdae191355be57a30306a

--- src/grenade/vx/execution_instance_node.cpp
+++ src/grenade/vx/execution_instance_node.cpp
@@ -1,10 +1,67 @@
 #include "grenade/vx/execution_instance_node.h"
 
-#include <log4cxx/logger.h>
 #include "halco/hicann-dls/vx/v2/coordinates.h"
+#include "haldls/vx/arq.h"
+#include "haldls/vx/phy.h"
 #include "hate/timer.h"
 #include "stadls/vx/v2/playback_program_builder.h"
 #include "stadls/vx/v2/run.h"
+#include <sstream>
+#include <log4cxx/logger.h>
+
+namespace {
+
+template <typename Connection>
+void perform_post_fail_analysis(
+    log4cxx::Logger* logger,
+    Connection& connection,
+    stadls::vx::v2::PlaybackProgram const& dead_program)
+{
+	using namespace halco::common;
+	using namespace halco::hicann_dls::vx::v2;
+	using namespace haldls::vx::v2;
+	using namespace stadls::vx::v2;
+
+	{
+		std::stringstream ss;
+		ss << "perform_post_fail_analysis(): Experiment failed, ";
+		bool empty_notifications = dead_program.get_highspeed_link_notifications().empty();
+		if (empty_notifications) {
+			ss << "did not receive any highspeed link notifications.";
+		} else {
+			ss << "reporting received notifications:\n";
+			for (auto const& noti : dead_program.get_highspeed_link_notifications()) {
+				ss << noti << "\n";
+			}
+		}
+		LOG4CXX_ERROR(logger, ss.str());
+	}
+
+	{
+		// perform post-mortem reads
+		PlaybackProgramBuilder builder;
+
+		// perform stat readout at the end of the experiment
+		auto ticket_arq = builder.read(HicannARQStatusOnFPGA());
+
+		std::vector<PlaybackProgram::ContainerTicket<PhyStatus>> tickets_phy;
+		for (auto coord : iter_all<PhyStatusOnFPGA>()) {
+			tickets_phy.emplace_back(builder.read(coord));
+		}
+
+		run(connection, builder.done());
+
+		std::stringstream ss;
+		ss << "perform_post_fail_analysis(): Experiment failed, reading post-mortem status.";
+		ss << ticket_arq.get() << "\n";
+		for (auto ticket_phy : tickets_phy) {
+			ss << ticket_phy.get() << "\n";
+		}
+		LOG4CXX_ERROR(logger, ss.str());
+	}
+}
+
+} // namespace
 
 namespace grenade::vx {
 
@@ -50,7 +107,13 @@ void ExecutionInstanceNode::operator()(tbb::flow::continue_msg)
 	if (!program.empty()) {
 		std::lock_guard lock(continuous_chunked_program_execution_mutex);
 		for (auto& p : program) {
-			stadls::vx::v2::run(connection, p);
+			try {
+				stadls::vx::v2::run(connection, p);
+			} catch (std::runtime_error const&) {
+				// TODO: use specific exception for fisch run() fails, cf. task #3724
+				perform_post_fail_analysis(logger, connection, p);
+				throw;
+			}
 		}
 	}
 	LOG4CXX_TRACE(
