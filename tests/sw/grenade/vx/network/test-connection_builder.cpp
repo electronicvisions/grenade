843dac87885db57a06f0b4111d35bcc55f323233
Philipp Spilger <philipp.spilger@kip.uni-heidelberg.de>
2021-06-04 16:17:31 +0200

    Add more tests for projection-overlap check
    Change-Id: Ic67664b5be9dbd17eef718daa55c7a6674f74e82

--- tests/sw/grenade/vx/network/test-connection_builder.cpp
+++ tests/sw/grenade/vx/network/test-connection_builder.cpp
@@ -12,6 +12,7 @@ TEST(build_routing, ProjectionOverlap)
 	NetworkBuilder builder;
 
 	Population population({AtomicNeuronOnDLS(Enum(0)), AtomicNeuronOnDLS(Enum(1))}, {true, true});
+	ExternalPopulation external_population(2);
 
 	// no overlap
 	auto descriptor = builder.add(population);
@@ -29,7 +30,7 @@ TEST(build_routing, ProjectionOverlap)
 	auto network = builder.done();
 	EXPECT_NO_THROW(grenade::vx::network::build_routing(network));
 
-	// overlap
+	// same projection
 	descriptor = builder.add(population);
 
 	Projection projection_3(
@@ -38,6 +39,72 @@ TEST(build_routing, ProjectionOverlap)
 
 	builder.add(projection_3);
 	builder.add(projection_3);
+
+	network = builder.done();
+	EXPECT_THROW(grenade::vx::network::build_routing(network), std::runtime_error);
+
+	// overlap
+	descriptor = builder.add(population);
+
+	Projection projection_4(
+	    Projection::ReceptorType::excitatory,
+	    {{0, 0, Projection::Connection::Weight()}, {1, 1, Projection::Connection::Weight()}},
+	    descriptor, descriptor);
+	Projection projection_5(
+	    Projection::ReceptorType::excitatory, {{1, 1, Projection::Connection::Weight()}},
+	    descriptor, descriptor);
+
+	builder.add(projection_4);
+	builder.add(projection_5);
+
+	network = builder.done();
+	EXPECT_THROW(grenade::vx::network::build_routing(network), std::runtime_error);
+
+	// from external no overlap
+	auto input_descriptor = builder.add(external_population);
+	descriptor = builder.add(population);
+
+	projection_1 = Projection(
+	    Projection::ReceptorType::excitatory, {{0, 0, Projection::Connection::Weight()}},
+	    input_descriptor, descriptor);
+	projection_2 = Projection(
+	    Projection::ReceptorType::excitatory, {{1, 1, Projection::Connection::Weight()}},
+	    input_descriptor, descriptor);
+
+	builder.add(projection_1);
+	builder.add(projection_2);
+
+	network = builder.done();
+	EXPECT_NO_THROW(grenade::vx::network::build_routing(network));
+
+	// from external same projection
+	input_descriptor = builder.add(external_population);
+	descriptor = builder.add(population);
+
+	projection_3 = Projection(
+	    Projection::ReceptorType::excitatory, {{0, 0, Projection::Connection::Weight()}},
+	    input_descriptor, descriptor);
+
+	builder.add(projection_3);
+	builder.add(projection_3);
+
+	network = builder.done();
+	EXPECT_THROW(grenade::vx::network::build_routing(network), std::runtime_error);
+
+	// from external overlap
+	input_descriptor = builder.add(external_population);
+	descriptor = builder.add(population);
+
+	projection_4 = Projection(
+	    Projection::ReceptorType::excitatory,
+	    {{0, 0, Projection::Connection::Weight()}, {1, 1, Projection::Connection::Weight()}},
+	    input_descriptor, descriptor);
+	projection_5 = Projection(
+	    Projection::ReceptorType::excitatory, {{1, 1, Projection::Connection::Weight()}},
+	    input_descriptor, descriptor);
+
+	builder.add(projection_4);
+	builder.add(projection_5);
 
 	network = builder.done();
 	EXPECT_THROW(grenade::vx::network::build_routing(network), std::runtime_error);
