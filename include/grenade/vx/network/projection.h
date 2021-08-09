782c7005247cf7bed97f75d221bed98c3126d193
Philipp Spilger <philipp.spilger@kip.uni-heidelberg.de>
2021-08-09 11:19:21 +0200

    Add from_numpy to Projection
    Change-Id: I2b1a4b3a3b2bf90d405be6efe329494cd8677edd

--- include/grenade/vx/network/projection.h
+++ include/grenade/vx/network/projection.h
@@ -6,6 +6,9 @@
 #include "hate/visibility.h"
 #include "lola/vx/v2/synapse.h"
 #include <vector>
+#if defined(__GENPYBIND__) || defined(__GENPYBIND_GENERATED__)
+#include <pybind11/numpy.h>
+#endif
 
 namespace grenade::vx GENPYBIND_TAG_GRENADE_VX {
 
@@ -64,6 +67,39 @@ struct GENPYBIND(visible) Projection
 	    Connections&& connections,
 	    PopulationDescriptor population_pre,
 	    PopulationDescriptor population_post) SYMBOL_VISIBLE;
+
+	GENPYBIND_MANUAL({
+		using namespace grenade::vx::network;
+
+		auto const from_numpy = [](GENPYBIND_PARENT_TYPE& self,
+		                           Projection::ReceptorType const receptor_type,
+		                           pybind11::array_t<size_t> const& pyconnections,
+		                           PopulationDescriptor const population_pre,
+		                           PopulationDescriptor const population_post) {
+			if (pyconnections.ndim() != 2) {
+				throw std::runtime_error("Expected connections array to be of dimension 2.");
+			}
+			auto const shape = std::vector<size_t>{pyconnections.shape(),
+			                                       pyconnections.shape() + pyconnections.ndim()};
+			if (shape.at(1) != 3) {
+				throw std::runtime_error("Expected connections array second dimension to be of "
+				                         "size 3 (index_pre, index_post, weight).");
+			}
+			self.connections.resize(shape.at(0));
+			auto const data = pyconnections.unchecked();
+			for (size_t i = 0; i < self.connections.size(); ++i) {
+				auto& lconn = self.connections.at(i);
+				lconn.index_pre = data(i, 0);
+				lconn.index_post = data(i, 1);
+				lconn.weight = Projection::Connection::Weight(data(i, 2));
+			}
+			self.receptor_type = receptor_type;
+			self.population_pre = population_pre;
+			self.population_post = population_post;
+		};
+
+		parent.def("from_numpy", from_numpy);
+	})
 
 	bool operator==(Projection const& other) const SYMBOL_VISIBLE;
 	bool operator!=(Projection const& other) const SYMBOL_VISIBLE;
