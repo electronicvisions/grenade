f6a915bd1b08f5f149c9e41773927f227d146176
Philipp Spilger <philipp.spilger@kip.uni-heidelberg.de>
2021-08-18 09:13:28 +0200

    Add test for TimedSpike
    Change-Id: Ibee8cf0547e5f1a5f8e0686aee65b1f56f6f6fe4

---
+++ tests/sw/grenade/vx/test-event.cpp
@@ -0,0 +1,27 @@
+#include <gtest/gtest.h>
+
+#include "grenade/vx/event.h"
+
+using namespace grenade::vx;
+
+TEST(TimedSpike, General)
+{
+	TimedSpike spike(
+	    TimedSpike::Time(123), haldls::vx::v2::SpikePack1ToChip({haldls::vx::v2::SpikeLabel(456)}));
+
+	EXPECT_EQ(spike.time, TimedSpike::Time(123));
+	EXPECT_TRUE(std::holds_alternative<haldls::vx::v2::SpikePack1ToChip>(spike.payload));
+	EXPECT_EQ(
+	    std::get<haldls::vx::v2::SpikePack1ToChip>(spike.payload),
+	    haldls::vx::v2::SpikePack1ToChip({haldls::vx::v2::SpikeLabel(456)}));
+
+	TimedSpike spike_other = spike;
+	spike_other.time = TimedSpike::Time(321);
+	EXPECT_NE(spike, spike_other);
+	spike_other.time = spike.time;
+	spike_other.payload = haldls::vx::v2::SpikePack1ToChip({haldls::vx::v2::SpikeLabel(654)});
+	EXPECT_NE(spike, spike_other);
+
+	TimedSpike spike_copy = spike;
+	EXPECT_EQ(spike, spike_copy);
+}
