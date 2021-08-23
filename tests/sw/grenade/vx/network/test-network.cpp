8b06c8ede458d0071d9bee7610c7ba5664042627
Philipp Spilger <philipp.spilger@kip.uni-heidelberg.de>
2021-08-23 12:17:36 +0200

    Add test for network::Network
    Change-Id: I933681633b7da7c9e08e739f5b8ae5f14c3d36d9

---
+++ tests/sw/grenade/vx/network/test-network.cpp
@@ -0,0 +1,51 @@
+#include <gtest/gtest.h>
+
+#include "grenade/vx/network/madc_recording.h"
+#include "grenade/vx/network/network.h"
+#include "grenade/vx/network/population.h"
+#include "grenade/vx/network/projection.h"
+
+using namespace grenade::vx::network;
+using namespace halco::hicann_dls::vx::v2;
+using namespace halco::common;
+
+TEST(network_Network, General)
+{
+	Network network_a{{{PopulationDescriptor(1), ExternalPopulation(2)}},
+	                  {{ProjectionDescriptor(3),
+	                    Projection(
+	                        Projection::ReceptorType::excitatory,
+	                        {Projection::Connection(4, 5, Projection::Connection::Weight(6))},
+	                        PopulationDescriptor(1), PopulationDescriptor(1))}},
+	                  MADCRecording{}};
+
+	Network network_b{{{PopulationDescriptor(2), ExternalPopulation(3)}},
+	                  {{ProjectionDescriptor(3),
+	                    Projection(
+	                        Projection::ReceptorType::excitatory,
+	                        {Projection::Connection(4, 5, Projection::Connection::Weight(6))},
+	                        PopulationDescriptor(1), PopulationDescriptor(1))}},
+	                  MADCRecording{}};
+
+	Network network_c{{{PopulationDescriptor(1), ExternalPopulation(2)}},
+	                  {{ProjectionDescriptor(4),
+	                    Projection(
+	                        Projection::ReceptorType::excitatory,
+	                        {Projection::Connection(4, 5, Projection::Connection::Weight(6))},
+	                        PopulationDescriptor(1), PopulationDescriptor(1))}},
+	                  MADCRecording{}};
+
+	Network network_d{{{PopulationDescriptor(1), ExternalPopulation(2)}},
+	                  {{ProjectionDescriptor(3),
+	                    Projection(
+	                        Projection::ReceptorType::excitatory,
+	                        {Projection::Connection(4, 5, Projection::Connection::Weight(6))},
+	                        PopulationDescriptor(1), PopulationDescriptor(1))}},
+	                  std::nullopt};
+
+	Network network_copy(network_a);
+	EXPECT_EQ(network_a, network_copy);
+	EXPECT_NE(network_b, network_copy);
+	EXPECT_NE(network_c, network_copy);
+	EXPECT_NE(network_d, network_copy);
+}
