fee747ed3182d4c0e450db128a665d054c1d238e
Philipp Spilger <philipp.spilger@kip.uni-heidelberg.de>
2021-09-09 11:49:20 +0200

    Reduce built serialization archives to JSON and PortableBinary
    Depends-On: 15488,15755,15760,15815
    Change-Id: I41520ac2e2eb417aadc1b2b4d3912a6e3cb75788

--- include/grenade/cerealization.h
+++ include/grenade/cerealization.h
@@ -3,17 +3,10 @@
 
 #define EXPLICIT_INSTANTIATE_CEREAL_LOAD_SAVE(CLASS_NAME)                                          \
 	template SYMBOL_VISIBLE void CLASS_NAME ::load(                                                \
-	    cereal::BinaryInputArchive&, std::uint32_t const);                                         \
-	template SYMBOL_VISIBLE void CLASS_NAME ::load(                                                \
 	    cereal::JSONInputArchive&, std::uint32_t const);                                           \
 	template SYMBOL_VISIBLE void CLASS_NAME ::load(                                                \
 	    cereal::PortableBinaryInputArchive&, std::uint32_t const);                                 \
-	template SYMBOL_VISIBLE void CLASS_NAME ::load(cereal::XMLInputArchive&, std::uint32_t const); \
-	template SYMBOL_VISIBLE void CLASS_NAME ::save(                                                \
-	    cereal::BinaryOutputArchive&, std::uint32_t const) const;                                  \
 	template SYMBOL_VISIBLE void CLASS_NAME ::save(                                                \
 	    cereal::JSONOutputArchive&, std::uint32_t const) const;                                    \
 	template SYMBOL_VISIBLE void CLASS_NAME ::save(                                                \
-	    cereal::PortableBinaryOutputArchive&, std::uint32_t const) const;                          \
-	template SYMBOL_VISIBLE void CLASS_NAME ::save(cereal::XMLOutputArchive&, std::uint32_t const) \
-	    const;
+	    cereal::PortableBinaryOutputArchive&, std::uint32_t const) const;
