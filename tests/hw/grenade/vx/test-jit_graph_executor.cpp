186e5c9888337d5a8578ee8bfa237b98c466d261
Philipp Spilger <philipp.spilger@kip.uni-heidelberg.de>
2023-06-07 10:41:07 +0200

    fix: Relax duration expectation in JITGraphExecutor.ConcurrentUsage test
    * the one execution of the static config setup just might occasionally
      require more time
    
    Change-Id: Ib545312b8b83aab1f2b2afe0e62e04e9cc6ff66f

--- tests/hw/grenade/vx/test-jit_graph_executor.cpp
+++ tests/hw/grenade/vx/test-jit_graph_executor.cpp
@@ -228,13 +228,13 @@ TEST(JITGraphExecutor, ConcurrentUsage)
 		    data.execution_time_info->execution_duration_per_hardware.at(
 		        halco::hicann_dls::vx::v3::DLSGlobal()),
 		    std::chrono::milliseconds(10));
-		// expect no more than 200ms per execution
+		// expect no more than 400ms per execution
 		// since we don't know which execution will be the first, we can't assert, that this one
 		// requires more time due to applying the initial config
 		EXPECT_LE(
 		    data.execution_time_info->execution_duration_per_hardware.at(
 		        halco::hicann_dls::vx::v3::DLSGlobal()),
-		    std::chrono::milliseconds(200));
+		    std::chrono::milliseconds(400));
 	}
 
 	// expect no more than three times the realtime runtime as duration
