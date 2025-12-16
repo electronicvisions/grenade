af4ea5026ef112eb9141bb12f11ed64a86616b7f
David Baumeister <david.baumeister@kip.uni-heidelberg.de>
2025-12-16 09:32:30 +0000

    fix: Cleanup of docstrings in MC-Environment
    Change-Id: I73de109ddf092b18f7a88272240a8e0712ce61a0

--- include/grenade/vx/network/abstract/multicompartment_environment.h
+++ include/grenade/vx/network/abstract/multicompartment_environment.h
@@ -11,25 +11,53 @@ namespace abstract GENPYBIND_TAG_GRENADE_VX_NETWORK {
 
 struct GENPYBIND(visible) SYMBOL_VISIBLE Environment
 {
-	// Adds information about number of synaptic inputs on a neuron.
+	/**
+	 * Add information about number of synaptic inputs on a neuron.
+	 * @param compartment Compartment for which synaptic input is added.
+	 * @param synaptic_input Type, Number and hemisphere of the synaptic inputs on the compartment.
+	 */
 	void add(
 	    CompartmentOnNeuron const& compartment, SynapticInputEnvironment const& synaptic_input);
+
+	/**
+	 * Add information about number of synaptic inputs on a neuron.
+	 * @param compartment Compartment for which synaptic input is added.
+	 * @param synaptic_inputs Multiple synaptic inputs containing information about the type, number
+	 * and hemisphere of the synaptic inputs.
+	 */
 	void add(
 	    CompartmentOnNeuron const& compartment,
 	    std::vector<dapr::PropertyHolder<SynapticInputEnvironment>> const& synaptic_inputs);
 
+	/**
+	 * Get all synaptic inputs defined on a compartment.
+	 * @param compartment Compartment for which to get the synaptic input information.
+	 */
 	std::vector<dapr::PropertyHolder<SynapticInputEnvironment>> get(
 	    CompartmentOnNeuron const& compartment) const;
 
-	// Adds a pair of recordable compartments to the list if only a single compartment is recorded
-	// nothing must be add here.
+	/**
+	 * Add a pair of recordable compartments.
+	 *
+	 * If only single compartments are recoreded nothing must be added here.
+	 *
+	 * @param compartment_a One compartment to be part of the recordable pair.
+	 * @param compartment_b Other compartment to be part of the recordable pair.
+	 */
 	void add_recordable(
 	    CompartmentOnNeuron const& compartment_a, CompartmentOnNeuron const& compartment_b);
 
-	// Gets all compartments that can be recorded with the given compartment.
+	/**
+	 * Get list of all compartments that can be recorded together with the given compartment.
+	 *
+	 * @param compartment Compartment for which compartments which can be recorded with it are
+	 * returned.
+	 */
 	std::vector<CompartmentOnNeuron> get_recordable(CompartmentOnNeuron const& compartment) const;
 
-	// Returns set of all recordable pairs.
+	/**
+	 * Get list of all compartment pairs that can be recorded.
+	 */
 	std::set<std::pair<CompartmentOnNeuron, CompartmentOnNeuron>> get_recordable_pairs() const;
 
 
