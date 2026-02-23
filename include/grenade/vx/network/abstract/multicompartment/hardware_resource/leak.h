df96036a932c7e141e59afc5a6b6d637c3a64d25
Philipp Spilger <philipp.spilger@kip.uni-heidelberg.de>
2026-02-23 12:51:56 +0100

    chore: Adapt to c/26123
    Depends-On: 26123
    Change-Id: Ieeb68cd54ec4b1a29e6a12da9b55dfb70ee17885

--- include/grenade/vx/network/abstract/multicompartment/hardware_resource/leak.h
+++ include/grenade/vx/network/abstract/multicompartment/hardware_resource/leak.h
@@ -3,18 +3,6 @@
 #include "grenade/vx/network/abstract/multicompartment/hardware_resource.h"
 
 namespace grenade::vx::network::abstract {
-struct HardwareResourceLeak;
-} // namespace grenade::vx::network::abstract
-
-namespace dapr {
-
-extern template struct SYMBOL_VISIBLE EmptyProperty<
-    grenade::vx::network::abstract::HardwareResourceLeak,
-    grenade::vx::network::abstract::HardwareResource>;
-
-} // namespace dapr
-
-namespace grenade::vx::network::abstract {
 
 struct SYMBOL_VISIBLE HardwareResourceLeak
     : public dapr::EmptyProperty<HardwareResourceLeak, HardwareResource>
