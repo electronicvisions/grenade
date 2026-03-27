42b8ace6ea1d891581086b316ea4cf4420ce46b1
David Baumeister <david.baumeister@kip.uni-heidelberg.de>
2026-03-27 12:44:45 +0000

    feat: Only run playback programs on chips on multichip setup where it is required
    Change-Id: I978da346dfd24ca618e4055a984cf93de6083da3

--- src/grenade/vx/execution/backend/stateful_connection_run.cpp
+++ src/grenade/vx/execution/backend/stateful_connection_run.cpp
@@ -8,6 +8,7 @@
 #include "grenade/vx/execution/detail/generator/get_state.h"
 #include "grenade/vx/execution/detail/generator/ppu.h"
 #include "hate/timer.h"
+#include <functional>
 #include <mutex>
 #include <ranges>
 #include <stdexcept>
@@ -50,26 +51,28 @@ RunTimeInfo run(StatefulConnection& connection, PlaybackProgram& program)
 		            "contiguity can't be guaranteed.");
 	}
 
-	bool const differential_config = std::ranges::all_of(
-	    connection.get_enable_differential_config(), [](bool const value) { return value; });
+	std::vector<bool> const differential_config = connection.get_enable_differential_config();
 
 	// Playback programs to get state.
 	hate::Timer const read_timer;
 	std::vector<stadls::vx::v3::PlaybackProgram> get_state_programs;
 	std::map<grenade::vx::common::ChipOnConnection, execution::detail::generator::GetState::Result>
 	    get_state_results;
-	if (differential_config) {
-		for (auto const& [chip, local_program] : program.chips) {
-			if (local_program.ppu_symbols) {
-				auto get_state = stadls::vx::v3::generate(
-				    execution::detail::generator::GetState(local_program.ppu_symbols.has_value()));
-				get_state_programs.emplace_back(get_state.builder.done());
-				get_state_results[chip] = get_state.result;
-			} else {
-				get_state_programs.emplace_back(stadls::vx::v3::PlaybackProgram());
-			}
+
+	for (size_t i = 0; i < chips_on_connection.size(); i++) {
+		auto const& chip = chips_on_connection.at(i);
+		auto const& local_program = program.chips.at(chip);
+
+		if (differential_config.at(i) && local_program.ppu_symbols) {
+			auto get_state = stadls::vx::v3::generate(
+			    execution::detail::generator::GetState(local_program.ppu_symbols.has_value()));
+			get_state_programs.emplace_back(get_state.builder.done());
+			get_state_results[chip] = std::move(get_state.result);
+		} else {
+			get_state_programs.emplace_back(stadls::vx::v3::PlaybackProgram());
 		}
 	}
+
 	LOG4CXX_TRACE(
 	    logger, "Generated playback program for read-back of PPU alterations in "
 	                << read_timer.print() << ".");
@@ -80,51 +83,50 @@ RunTimeInfo run(StatefulConnection& connection, PlaybackProgram& program)
 	std::unique_lock<std::mutex> connection_lock(*connection.m_mutex, std::defer_lock);
 	// exclusive access to connection.m_state_storage and initialized connection required from here
 	// in differential mode
-	if (differential_config) {
+	if (std::ranges::any_of(differential_config, std::identity{})) {
 		connection_lock.lock();
 	}
 
 	hate::Timer const initial_config_program_timer;
-	bool is_fresh_connection_config = false;
-	for (auto const& [chip, local_program] : program.chips) {
-		if (connection.m_configs.at(chip).get_is_fresh()) {
-			is_fresh_connection_config = true;
-		}
+	std::vector<bool> is_fresh_connection_config;
+	std::vector<bool> enforce_base;
+	std::vector<bool> has_capmem_changes;
+	std::vector<bool> nothing_changed;
+	for (size_t i = 0; i < chips_on_connection.size(); i++) {
+		auto const& chip = chips_on_connection.at(i);
+		auto const& local_program = program.chips.at(chip);
+		is_fresh_connection_config.emplace_back(connection.m_configs.at(chip).get_is_fresh());
+
 		connection.m_configs.at(chip).set_system(local_program.system_configs[0], true);
 		if (local_program.external_ppu_dram_memory_config) {
 			connection.m_configs.at(chip).set_external_ppu_dram_memory(
 			    local_program.external_ppu_dram_memory_config);
 		}
+
+		enforce_base.emplace_back(
+		    !differential_config.at(i) || is_fresh_connection_config.at(i) ||
+		    !local_program.pre_initial_config_hook.empty());
+
+		has_capmem_changes.emplace_back(
+		    enforce_base.at(i) || connection.m_configs.at(chip).get_differential_changes_capmem());
+
+		nothing_changed.emplace_back(
+		    differential_config.at(i) && !is_fresh_connection_config.at(i) &&
+		    !connection.m_configs.at(chip).get_has_differential() &&
+		    local_program.pre_initial_config_hook.empty());
 	}
-
-	bool const enforce_base = !differential_config || is_fresh_connection_config ||
-	                          !std::ranges::all_of(program.chips, [](auto const& pair) {
-		                          return pair.second.pre_initial_config_hook.empty();
-	                          });
-	bool const has_capmem_changes =
-	    enforce_base || std::ranges::any_of(program.chips, [&connection](auto const& pair) {
-		    return connection.m_configs.at(pair.first).get_differential_changes_capmem();
-	    });
-	bool const nothing_changed =
-	    differential_config && !is_fresh_connection_config &&
-	    !std::ranges::any_of(
-	        program.chips,
-	        [&connection](auto const& pair) {
-		        return connection.m_configs.at(pair.first).get_has_differential();
-	        }) &&
-	    std::ranges::all_of(program.chips, [](auto const& pair) {
-		    return pair.second.pre_initial_config_hook.empty();
-	    });
-
 
 	std::vector<stadls::vx::PlaybackProgram> base_programs;
 	std::vector<stadls::vx::PlaybackProgram> differential_programs;
-	for (auto& [chip, local_program] : program.chips) {
+	for (size_t i = 0; i < chips_on_connection.size(); i++) {
+		auto const& chip = chips_on_connection.at(i);
+		auto& local_program = program.chips.at(chip);
+
 		stadls::vx::v3::PlaybackProgramBuilder base_builder;
 		base_builder.merge_back(local_program.pre_initial_config_hook);
 
 		stadls::vx::v3::PlaybackProgramBuilder differential_builder;
-		if (!nothing_changed) {
+		if (!nothing_changed.at(i)) {
 			base_builder.write(
 			    detail::StatefulChipConfigBaseCoordinate(),
 			    detail::StatefulChipConfigBase(connection.m_configs.at(chip)));
@@ -193,9 +195,8 @@ RunTimeInfo run(StatefulConnection& connection, PlaybackProgram& program)
 			}
 		}
 
-
 		// only lock execution section for non-differential config mode
-		if (!differential_config) {
+		if (std::ranges::none_of(differential_config, std::identity{})) {
 			connection_lock.lock();
 		}
 
@@ -205,8 +206,10 @@ RunTimeInfo run(StatefulConnection& connection, PlaybackProgram& program)
 		    [](auto const& chip_time_info) { return chip_time_info.execution_duration; });
 
 		// Only if something changed (re-)set base and differential reinit
-		if (!nothing_changed) {
-			connection.m_reinit_base.set(std::move(base_programs), std::nullopt, enforce_base);
+		if (!std::ranges::all_of(nothing_changed, std::identity{})) {
+			connection.m_reinit_base.set(
+			    std::move(base_programs), std::nullopt,
+			    std::ranges::any_of(enforce_base, std::identity{}));
 
 			// Only enforce when not empty to support non-differential mode.
 			// In differential mode it is always enforced on changes.
@@ -230,7 +233,8 @@ RunTimeInfo run(StatefulConnection& connection, PlaybackProgram& program)
 		    stadls::vx::v3::generate(execution::detail::generator::CapMemSettlingWait())
 		        .builder.done());
 		connection.m_reinit_capmem_settling_wait.set(
-		    std::move(capmem_settling_waits), std::nullopt, has_capmem_changes);
+		    std::move(capmem_settling_waits), std::nullopt,
+		    std::ranges::any_of(has_capmem_changes, std::identity{}));
 
 		// Always write (PPU) trigger reinit and enforce when not empty, i.e. when PPUs are
 		// used.
@@ -271,7 +275,7 @@ RunTimeInfo run(StatefulConnection& connection, PlaybackProgram& program)
 		}
 
 		// unlock execution section for non-differential config mode
-		if (!differential_config) {
+		if (std::ranges::none_of(differential_config, std::identity{})) {
 			std::ranges::transform(
 			    connection.get_time_info(), connection_execution_duration_after.begin(),
 			    [](auto const& chip_time_info) { return chip_time_info.execution_duration; });
@@ -283,7 +287,10 @@ RunTimeInfo run(StatefulConnection& connection, PlaybackProgram& program)
 
 	// update connection state
 	hate::Timer const update_state_timer;
-	for (auto const& [chip, local_program] : program.chips) {
+	for (size_t i = 0; i < chips_on_connection.size(); i++) {
+		auto const& chip = chips_on_connection.at(i);
+		auto const& local_program = program.chips.at(chip);
+
 		if (local_program.system_configs.size() > 1 || local_program.ppu_symbols) {
 			auto current_config =
 			    local_program.system_configs[local_program.system_configs.size() - 1];
@@ -299,14 +306,15 @@ RunTimeInfo run(StatefulConnection& connection, PlaybackProgram& program)
 				// TODO: create reset mechanism in class
 
 				connection.m_configs.at(chip).reset();
-				connection.m_configs.at(chip).set_enable_differential_config(differential_config);
+				connection.m_configs.at(chip).set_enable_differential_config(
+				    differential_config.at(i));
 			}
 		}
 	}
 	LOG4CXX_TRACE(logger, "Updated connection state in " << update_state_timer.print() << ".");
 
 	// unlock execution section for differential config mode
-	if (differential_config) {
+	if (std::ranges::any_of(differential_config, std::identity{})) {
 		if (!std::ranges::all_of(
 		        program.chips, [](auto const& pair) { return pair.second.programs.empty(); }) ||
 		    !std::ranges::all_of(
