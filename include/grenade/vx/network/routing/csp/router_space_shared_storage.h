7ea6d0b583b64749124172ae551eacf409b04a6c
David Baumeister <david.baumeister@kip.uni-heidelberg.de>
2026-04-16 06:35:31 +0000

    feat: Add coordinates for routing vertices
    Change-Id: I5eedd969c2c76dd00e33fa2755d9b4f4eb696b3b

--- include/grenade/vx/network/routing/csp/router_space_shared_storage.h
+++ include/grenade/vx/network/routing/csp/router_space_shared_storage.h
@@ -11,6 +11,28 @@ namespace grenade::vx::network::routing::csp {
  */
 struct RouterSpaceSharedStorage
 {
+	struct SpatialVertexInformation
+	{
+		using Coordinates = std::vector<size_t>;
+
+		/**
+		 * Get coordinates of vertex.
+		 * @param vertex Vertex to get coordinates for.
+		 */
+		Coordinates get_coordinates(grenade::common::VertexOnTopology const& vertex) const;
+
+		/**
+		 * Set coordinates of vertex.
+		 * @param vertex Vertex to set coordinates for.
+		 * @param coordinates Coordinates to be set.
+		 */
+		void set_coordinates(
+		    grenade::common::VertexOnTopology const& vertex, Coordinates const& coordinates);
+
+	private:
+		std::map<grenade::common::VertexOnTopology, std::vector<size_t>> m_coordinates;
+	} spatial_vertex_information;
+
 	RouterSpaceSharedStorage();
 
 	log4cxx::LoggerPtr logger;
--- src/grenade/vx/network/routing/csp/router_space_shared_storage.cpp
+++ src/grenade/vx/network/routing/csp/router_space_shared_storage.cpp
@@ -1,10 +1,30 @@
 #include "grenade/vx/network/routing/csp/router_space_shared_storage.h"
 
+#include <sstream>
+
 namespace grenade::vx::network::routing::csp {
 
 RouterSpaceSharedStorage::RouterSpaceSharedStorage() :
     logger(log4cxx::Logger::getLogger("grenade.network.routing.csp.RouterSpaceSharedStorage"))
 {
+}
+
+RouterSpaceSharedStorage::SpatialVertexInformation::Coordinates
+RouterSpaceSharedStorage::SpatialVertexInformation::get_coordinates(
+    grenade::common::VertexOnTopology const& vertex) const
+{
+	if (!m_coordinates.contains(vertex)) {
+		std::stringstream ss;
+		ss << "No coordinates for " << vertex << " given.";
+		throw std::range_error(ss.str());
+	}
+	return m_coordinates.at(vertex);
+}
+
+void RouterSpaceSharedStorage::SpatialVertexInformation::set_coordinates(
+    grenade::common::VertexOnTopology const& vertex, Coordinates const& coordinates)
+{
+	m_coordinates[vertex] = coordinates;
 }
 
 } // namespace grenade::vx::network::routing::csp
